e7a509de4c5014f266e3e5d940d4feae9547e665
Srzchz <charlito.nodalo225@gmail.com>
2026-08-24 18:46:29 +0800

    add subject

--- NODALO_MERNSTACK/src/App.jsx
+++ NODALO_MERNSTACK/src/App.jsx
@@ -1,4 +1,5 @@
 import StudentCard from "./components/studentcard.jsx";
+import SubjectCard from "./components/subject.jsx";
 
 function App() {
   return (
@@ -6,6 +7,9 @@ function App() {
       <StudentCard name="Charles" age={23} studentNumber="202400926" course="Information Technologies" />
       <StudentCard name="John Yevs Bryan" age={20} studentNumber="202404436" course="Information Technologies" />
       <StudentCard name="Jhomer" age={21} studentNumber="202405742" course="Information Technologies" />
+
+      <SubjectCard subjectCode="DCIT 26" subjectName="Application and Development" schedule="Mon 5:30 PM - 7:00 PM, Tue 11:00 AM - 1:00 PM" />
+
     </div>
   );
 }
