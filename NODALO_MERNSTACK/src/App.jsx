95df3310319df6b140b711f18a7110d117ed8b9f
Srzchz <charlito.nodalo225@gmail.com>
2026-09-14 19:05:18 +0800

    addstudents

--- NODALO_MERNSTACK/src/App.jsx
+++ NODALO_MERNSTACK/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/home.jsx";
 import Student from "./pages/student.jsx";
 import StudentDetails from "./pages/studentDetails.jsx";
 import Navbar from "./components/navbar.jsx";
+import AddStudents from "./pages/addStudents.jsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
             <Route path="/" element={<Home/>} />
             <Route path="/student" element={<Student/>} />
             <Route path="/student/:id" element={<StudentDetails />} />
+            <Route path="/add-student" element={<AddStudents />} />
         </Routes>
     </BrowserRouter>
   );
