33809bc49b9e0d04bb3a89233a32e54438329d51
Srzchz <charlito.nodalo225@gmail.com>
2026-09-21 17:19:55 +0800

    add student function

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,6 +6,7 @@ export default function Navbar() {
             <ul className="flex space-x-4 bg-gray-600 p-4 text-white">
                 <Link to="/">Home</Link>
                 <Link to="/student">Student</Link>
+                <Link to="/add-student">Add Student</Link>
             </ul>
         </nav>
     );
--- src/components/studentcard.jsx
+++ src/components/studentcard.jsx
@@ -1,10 +1,13 @@
 import { Link, Route } from "react-router-dom";
 
-function studentCard({ id, name, age }) {
+function studentCard({ id, name, age, sex, course, courseDescription }) {
     return (
         <div className="align-center bg-gray-300 shadow-md rounded-lg p-4 m-4 max-w-sm mx-auto">
             <h2 className="text-xl font-semibold mb-2">{name}</h2>
-            <p className="text-gray-600 mb-1">Age: {age}</p><br/>
+            <p className="text-gray-600 mb-1">Age: {age}</p>
+            <p className="text-gray-600 mb-1">Sex: {sex}</p>
+            <p className="text-gray-600 mb-1">Course: {course}</p>
+            <p className="text-gray-600 mb-1">Course Description: {courseDescription}</p><br/>
             <Link to={`/student/${id}`} className="bg-gray-600 text-white py-2 px-4 rounded hover:bg-gray-800">
                 View Student Details
             </Link>
